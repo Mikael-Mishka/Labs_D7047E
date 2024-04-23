5b69026b7bf393bd80108c6e63ed65eba1986d1b
Jonathan Persson <Jon00per@gmail.com>
2024-04-23 11:12:52 +0200

    task 2.4 python fil

--- Lab 2/Task2.4.py
+++ Lab 2/Task2.4.py
@@ -1,1 +1,36 @@
-fggsgs
+
+from torchvision.datasets import MNIST
+
+
+
+
+
+#Deta spliting
+
+
+#traning_data
+#valedasun_data
+#test_data
+
+#CNN
+
+    #the CNN model
+
+
+    #model tranig
+        #trening step
+
+        #valedesun step
+
+
+#adversarial images
+
+
+
+
+
+
+
+
+
+
